77135392a63a1ab40bbdbeae02881df9f807f316
myzkey <miyazakiyasunari@gmail.com>
2024-12-18 09:46:18 +0900

    feat: add example pattern

--- examples/cloudflare/index.ts
+++ examples/cloudflare/index.ts
@@ -1,24 +1,42 @@
 import { youtubeapiEdge } from 'youtubeapi-edge'
 
 export default {
-  async fetch(_request: Request, context: { YOUTUBE_API_KEY: string }): Promise<Response> {
+  async fetch(
+    _request: Request,
+    context: { YOUTUBE_API_KEY: string },
+  ): Promise<Response> {
     const youtube = youtubeapiEdge({
       version: 'v3',
-      auth: context.YOUTUBE_API_KEY
+      auth: context.YOUTUBE_API_KEY,
     })
     try {
-      const result = await youtube.channels.list({
+      const channelList = await youtube.channels.list({
         part: 'snippet',
-        id: 'UCX6OQ3DkcsbYNE6H8uQQuVA'
+        id: 'UCX6OQ3DkcsbYNE6H8uQQuVA',
       })
-      return new Response(JSON.stringify(result), {
-        headers: { 'Content-Type': 'application/json' },
-      });
+      const videoList = await youtube.videos.list({
+        part: 'snippet',
+        chart: 'mostPopular',
+      })
+      const searchList = await youtube.search.list({
+        part: 'snippet',
+        q: 'hoge',
+      })
+      return new Response(
+        JSON.stringify({
+          channelList,
+          videoList,
+          searchList,
+        }),
+        {
+          headers: { 'Content-Type': 'application/json' },
+        },
+      )
     } catch (error) {
       return new Response(JSON.stringify({ error: error.message }), {
         status: 500,
         headers: { 'Content-Type': 'application/json' },
-      });
+      })
     }
   },
-};
+}
